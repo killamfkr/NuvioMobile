3929ad32a706c29625ba4012400946dad571ac9a
tapframe <85391825+tapframe@users.noreply.github.com>
2026-03-28 21:32:11 +0530

    feat: Update AndroidManifest and MainActivity for new theme and background color support

--- composeApp/src/androidMain/kotlin/com/nuvio/app/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/nuvio/app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.SystemBarStyle
 import com.nuvio.app.features.addons.AddonStorage
 import com.nuvio.app.features.library.LibraryStorage
 import com.nuvio.app.features.home.HomeCatalogSettingsStorage
@@ -15,8 +16,19 @@ import com.nuvio.app.features.watchprogress.WatchProgressStorage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(
+                scrim = 0xFF020404.toInt(),
+            ),
+            navigationBarStyle = SystemBarStyle.dark(
+                scrim = 0xFF020404.toInt(),
+            ),
+        )
         super.onCreate(savedInstanceState)
+        window.setBackgroundDrawableResource(R.color.nuvio_background)
+        window.navigationBarColor = getColor(R.color.nuvio_background)
+        window.statusBarColor = getColor(R.color.nuvio_background)
+        window.isNavigationBarContrastEnforced = false
         AddonStorage.initialize(applicationContext)
         LibraryStorage.initialize(applicationContext)
         WatchedStorage.initialize(applicationContext)
